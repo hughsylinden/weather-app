e3b1c7432baff69f75adc036c9d223b65c57fcda
Hugh Kristiansen <kaarelinden@hotmail.com>
2021-11-27 15:52:46 +0000

    Updates

---
+++ precache-manifest.6ebd3802167d8705db20f7ba1c0dbf81.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f449bffcfb044069a24bffb927adf6",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "af847f861ca43914165d",
+    "url": "/weather-app/static/css/2.66e30163.chunk.css"
+  },
+  {
+    "revision": "ad443ae6833ba8665e42",
+    "url": "/weather-app/static/css/main.f28878ff.chunk.css"
+  },
+  {
+    "revision": "af847f861ca43914165d",
+    "url": "/weather-app/static/js/2.01d25336.chunk.js"
+  },
+  {
+    "revision": "24e93c76dc91f8760ed6f36282803268",
+    "url": "/weather-app/static/js/2.01d25336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad443ae6833ba8665e42",
+    "url": "/weather-app/static/js/main.873b9d42.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "784874dface6e56e3bf637d05d675fe3",
+    "url": "/weather-app/static/media/arrow.784874df.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
